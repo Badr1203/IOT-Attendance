8d0bbdfb21084b427f0dabdb7367fcdb23e70c27
Badriddin <s321451@studenti.polito.it>
2025-05-30 09:12:07 +0500

    Template secrets.h file addae

---
+++ RFID_Attendance (ESP32)/src/secrets_template.h
@@ -0,0 +1,12 @@
+// secrets_template.h
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char *WIFI_SSID = "YOUR_WIFI_SSID";
+const char *WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+const char *MQTT_SERVER = "BROKER_IP";
+const int MQTT_PORT = 1883;
+const char *MQTT_USERNAME = "USERNAME";
+const char *MQTT_PASSWORD = "PASSWORD";
+
+#endif
